56c36208c2cbf3fe81bfaa086b6146d864addf0a
montero253 <oyuleidimontero@gmail.com>
2025-08-09 13:30:14 -0500

    palabras

---
+++ palabra.js
@@ -0,0 +1,7 @@
+let num1 = prompt("digite el primer numero")
+let num2 = prompt("digite el segundo numero")
+num1 = parseInt (num1);
+num2 = parseInt (num2);
+suma = num1 + num2;
+console.log(suma)
+alert("El resultado de la suma es: "+suma)
