e4caf477dbeafc6ce231fce622a6d95df718adbf
montero253 <oyuleidimontero@gmail.com>
2025-08-09 10:42:16 -0500

    oper

---
+++ operadores.js
@@ -0,0 +1,37 @@
+// suma;resta
+//
+let num1 = 4;
+var num2 = 8;
+let suma = num1 + num2;
+console.log("el resultado es"+ suma)
+// contador 
+let edad = 24;
+edad = edad + 1//edad++
+console.log("edad es:"+ edad)
+// resta
+let res1 = 7;
+let res2 = 8;
+console.log(res1 - res2);
+// contador resta
+let edad1 = 27;
+edad1--;
+console.log(edad1);
+// multiplicacion 
+let multi1 = 5;
+let multi2 = 10;
+let multiplicacion = multi1 * multi2;
+console.log("el resultado de la multiplicacion es: "+ multiplicacion)
+// division
+let div1 = 8;
+let div2 = 7;
+let division = div1 / div2;
+console.log("el resultado de la division es. "+ division)
+// resuido de division
+let residuo = multiplicacion / division;
+console.log("el residuo es: "+ residuo)
+
+
+
+
+
+ 
